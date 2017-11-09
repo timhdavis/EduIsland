f661e2b4feae86663e52492a19b037844f3952d2
sshingh67 <ssuppal67@outlook.com>
2017-11-08 23:51:50 -0800

    entity class for user

---
+++ src/main/java/webapp157A/User.java
@@ -0,0 +1,34 @@
+package webapp157A;
+
+import com.mysql.jdbc.TimeUtil;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+public class User {
+
+    private String userId;
+    private String password;
+    private Date dateJoined = new Timestamp(System.currentTimeMillis());
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Date getDatejoined() {
+        return dateJoined;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
